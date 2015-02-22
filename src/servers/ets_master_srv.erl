bec9b81e8bd632e83818c5598f73eac27fcba9bf
Angel Alvarez <angeljalvarezmiguel@gmail.com>
2015-02-22 23:15:33 +0100

    ETS master server
    Master Server is the heir for all ETS tables and provide services to
    restarting gen_*

---
+++ src/servers/ets_master_srv.erl
@@ -0,0 +1,88 @@
+%% TUI Service project
+%% (C) 2014 Angel J. Alvarez Miguel (Hydra DHCP project)
+%% (C) 2015 Angel J. ALvarez Miguel 
+
+-module(ets_master_srv).
+-author("angeljalvarezmiguel@gmail.com").
+-behaviour(gen_server).
+-define(SERVER, ?MODULE).
+
+%% ------------------------------------------------------------------
+%% API Function Exports
+%% ------------------------------------------------------------------
+
+-export([start_link/1]).
+
+%% ------------------------------------------------------------------
+%% gen_server Function Exports
+%% ------------------------------------------------------------------
+
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2,
+         terminate/2, code_change/3]).
+
+-record(st,{
+			tables = []      %% dict of {Name,Tid} this process knows about
+			}).
+
+%% ------------------------------------------------------------------
+%% API Function Definitions
+%% ------------------------------------------------------------------
+
+start_link(Opts) ->
+    gen_server:start_link({local, ?SERVER}, ?MODULE, Opts, []).
+
+%% ------------------------------------------------------------------
+%% gen_server Function Definitions
+%% ------------------------------------------------------------------
+
+init(_Opts) ->
+	io:format("[~p]: Init..\n", [?MODULE]),
+
+    {ok, #st{ tables = dict:new() }}.
+
+%% Tell a process the Tid of a table (if we know that)
+handle_call({get, Name}, From, #st{ tables = Tables} = State) ->
+	Result = case dict:find(Name, Tables) of
+			{ok, Value} -> 
+                            io:format("[~p]: Pid ~p asked for table: ~p, we sent ~p..\n", [?MODULE, From, Name, Value]),
+                            {ok, Value};
+			error       ->
+                            io:format("[~p]: Pid ~p asked for table: ~p, we sent ~p..\n", [?MODULE, From, Name, not_found]),
+                            {not_found, self()}
+			end,
+	{reply, Result, State};
+
+%% Remove a Table in case the process wants to close it
+handle_call({remove, Name}, _From, #st{ tables = Tables} = State) ->	
+    {reply, ok, State#st{ tables = dict:erase(Name,Tables) }};
+
+handle_call(_Request, _From, State) ->
+    {reply, ok, State}.
+
+handle_cast(_Msg, State) ->
+    {noreply, State}.
+
+
+%% Gain table ownership when some process screws up. Heirdata must contain the table Name
+handle_info({'ETS-TRANSFER', Tid, _FromPid, {name , Name}}, #st{ tables = Tables } = State) ->
+	{noreply, State#st{ tables = dict:update(Name, fun(X) -> X end, Tid, Tables)}};  
+
+%% Failed attept to gain a table ownership because we dont know the table's name 
+handle_info({'ETS-TRANSFER', _Tid, FromPid, _Whatever}, State) ->
+	io:format("[TABLE MASTER]: Sorry!, process ~p died, but we can not inherit its table\n", [FromPid]),
+	{noreply, State};  
+
+handle_info(Info, State) ->
+	io:format("[TABLE MASTER]: Sorry!, I received this: ~p and I dont like it...\n", [Info]),
+    {noreply, State}.
+
+terminate(_Reason, _State) ->
+    ok.
+
+code_change(_OldVsn, State, _Extra) ->
+    {ok, State}.
+
+%% ------------------------------------------------------------------
+%% Internal Function Definitions
+%% ------------------------------------------------------------------
+
