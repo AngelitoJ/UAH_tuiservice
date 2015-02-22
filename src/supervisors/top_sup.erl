5b80c78e37eabdf75c28f57166df33e68b625059
Angel Alvarez <angeljalvarezmiguel@gmail.com>
2015-02-22 23:14:47 +0100

    Update top supervisor
    Add ETS master and DB server

--- src/supervisors/top_sup.erl
+++ src/supervisors/top_sup.erl
@@ -2,6 +2,7 @@
 %% (C) 2015 Angel J. Alvarez Miguel
 
 -module(top_sup).
+-author("angeljalvarezmiguel@gmail.com").
 
 -behaviour(supervisor).
 
@@ -26,10 +27,15 @@ start_link(Opts) ->  %% Opts is a property list
 %% Supervisor callbacks
 %% ===================================================================
 
-init(_Opts) ->
-    ChildrenSpec = [],
+init(Opts) ->
+    ChildrenSpec = [
+    				 ?CHILDWRK(ets_master_srv, Opts)   %% ETS tables master server
+    				,?CHILDWRK(db_srv, Opts)          %% User DB server    
+    				],
     io:format("[~p]: Init, I got ~p children to spawn..\n", [?MODULE,length(ChildrenSpec)]),
 
     {ok, { {one_for_one, 5, 10},
     							ChildrenSpec } }.
+
+
 
